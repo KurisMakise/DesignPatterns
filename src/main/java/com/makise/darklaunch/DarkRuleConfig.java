355b97addd628f2b4eddc368ec7928c524e23372
violet <569252295@qq.com>
2020-07-26 22:57:28 +0800

    初始化

--- src/main/java/com/makise/darklaunch/DarkRuleConfig.java
+++ src/main/java/com/makise/darklaunch/DarkRuleConfig.java
@@ -2,11 +2,12 @@ package com.makise.darklaunch;
 
 /**
  * 灰度配置
- *
+ * 
  * @author k.makise
  * @version 1.0
  * @date 2020/7/22 23:54
  */
+
 public class DarkRuleConfig {
     /**
      * features:
