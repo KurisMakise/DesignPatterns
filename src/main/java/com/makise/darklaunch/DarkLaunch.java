cb634760002139c43b51d3a9c6d192ecb4fe8e6f
violet <569252295@qq.com>
2020-07-26 22:43:02 +0800

    初始化

--- src/main/java/com/makise/darklaunch/DarkLaunch.java
+++ src/main/java/com/makise/darklaunch/DarkLaunch.java
@@ -8,4 +8,23 @@ package com.makise.darklaunch;
  * @date 2020/7/22 23:52
  */
 public class DarkLaunch {
+
+    /**
+     * in seconds
+     */
+    private static final int DEFAULT_RULE_UPDATE_INTERVAL = 60;
+
+    public DarkLaunch() {
+        this(DEFAULT_RULE_UPDATE_INTERVAL);
+    }
+
+    public DarkLaunch(int ruleUpdateInterval) {
+
+    }
+
+    private void loadRule() {
+
+    }
+
+
 }
--- src/main/java/com/makise/darklaunch/DarkRuleConfig.java
+++ src/main/java/com/makise/darklaunch/DarkRuleConfig.java
@@ -8,4 +8,18 @@ package com.makise.darklaunch;
  * @date 2020/7/22 23:54
  */
 public class DarkRuleConfig {
+    /**
+     * features:
+     * - key: call_newapi_getUserById
+     * enabled: true
+     * rule: {893,342,1020-1120,%30}
+     * - key: call_newapi_registerUser
+     * enabled: true
+     * rule: {1391198723, %10}
+     * - key: newalgo_loan
+     * enabled: true
+     * rule: {0-1000}
+     */
+
+
 }
